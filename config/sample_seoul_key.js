3e59474126a91f04ce6900689fa8d6563175a275
KimYoungHwan5521 <aorkslrdnjwm@naver.com>
2018-08-16 13:53:08 +0900

    first

---
+++ config/sample_seoul_key.js
@@ -0,0 +1,9 @@
+var seoul_api = {
+		
+		api_key : '567672536a616f7236306841487170',
+		api_url : 'http://openAPI.seoul.go.kr:8088/sample/xml/SearchPerformanceBySubjectService/1/5/1/',
+		api_name : 'SearchPerformanceBySubjectService'
+		
+}
+
+exports.seoul = seoul_api
